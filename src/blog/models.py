c32baef8ffdd70287c27da84f0c6c457fd14527d
Ahmet <scienceworker34@gmail.com>
2021-02-06 22:06:29 +0300

    updated model file

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+def user_directory_path(instance, filename):
+    return 'blog/{0}/{1}'.format(instance.author.id, filename)
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -16,7 +18,7 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to=user_directory_path, default='django.jpg')
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
