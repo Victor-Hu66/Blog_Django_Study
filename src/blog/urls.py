bb9771bde3cea506d817ee98692573d1bf6a6dfc
Ahmet <scienceworker34@gmail.com>
2021-02-14 20:13:34 +0300

    add pos_create

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, post_list, post_create
 
+app_name = "blog"
 urlpatterns = [
-    path("", home)
+    # path("", home),
+    path("", post_list, name="list"),
+    path("create/", post_create, name="create")
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
 
 def home(request):
     return HttpResponse("hello")
+
+def post_list(request):
+    qs = Post.objects.all()
+    context = {
+        "object_list":qs
+    }
+    return render(request, "blog/post_list.html", context)
+
+def post_create(request):
+    # form = PostForm(request.POST or None, request.FILES or None)
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            #form.save()
+            return redirect("blog:list")
+    context = {
+        'form' : form
+    }
+    return render(request, "blog/post_create.html", context)
